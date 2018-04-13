c9dd5971f1fcbc57959bd1e4074ecd1cb4788508
haguni02 <haguni17@gmail.com>
2018-04-13 13:45:25 +0900

    find textView

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -2,11 +2,11 @@ package edu.android.gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
-    int a;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
     }
 }
